0f8cafd941accc66e99227a835629679df3e0429
Charles Zhang <gzcharleszhang@hotmail.com>
2019-06-21 02:25:15 -0400

    social links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import Placeholder from "./placeholder.component";
 // import Timeline from "./timeline.component";
+import { Github, LinkedIn } from "./socials";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const App = () => {
-  return <Placeholder />;
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route exact path="/" component={Placeholder} />
+        <Route exact path="/github" component={Github} />
+        <Route exact path="/linkedin" component={LinkedIn} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/socials.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Github = () => {
+  window.location.assign("https://github.com/gzcharleszhang");
+  return null;
+};
+
+export const LinkedIn = () => {
+  window.location.assign("https://linkedin.com/in/gzcz");
+  return null;
+};
