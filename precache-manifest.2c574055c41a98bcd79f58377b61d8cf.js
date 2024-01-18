9b2464b5ffdadac360c0495e8898aea8433ff777
Charles Zhang <gzcharleszhang@hotmail.com>
2024-01-17 20:32:04 -0800

    Updates

---
+++ precache-manifest.2c574055c41a98bcd79f58377b61d8cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a61cf8d1dbd8367ff23f9fddd773937",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faa1e784ae9c917e7679",
+    "url": "/static/css/main.7e3b9b40.chunk.css"
+  },
+  {
+    "revision": "0d5644455cef0975382e",
+    "url": "/static/js/2.c89c4299.chunk.js"
+  },
+  {
+    "revision": "faa1e784ae9c917e7679",
+    "url": "/static/js/main.4ee2a880.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
